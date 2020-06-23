4358cf6f89f5fe0d70511de41265816204fc7c71
Davide Saporita <davide.saporita@gmail.com>
2020-06-23 17:03:58 +0200

    Added | Comments generated in 'users' show view

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,10 +4,16 @@
     <h3>{{ $user->name }} | Info, Posts & Comments</h1>
     <h4>Email: <a href="mailto:{{ $user->email }}">{{ $user->email }}</a></h3>
     <hr>
-    <h3>Posts by {{ $user->name }}</h3>
+    <h3>Posts created</h3>
     <ul>
         @foreach($user->posts as $post)
             <li><a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a> | Date: {{ $post->created_at }}</li>
+        @endforeach
+    </ul>
+    <h3>Comments added</h3>
+    <ul>
+        @foreach($user->comments as $comment)
+            <li><a href="{{ route('posts.show', $comment->post) }}">{{ $comment->title }} on post "{{ $comment->post->title }}"</a> | Date: {{ $post->created_at }}</li>
         @endforeach
     </ul>
     <hr>
