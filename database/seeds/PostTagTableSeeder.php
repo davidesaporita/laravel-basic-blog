fd80d94ad0993ede15533753ca78481b0d4bd231
Davide Saporita <davide.saporita@gmail.com>
2020-06-24 20:21:45 +0200

    Fixed | Seeding algorithm

--- database/seeds/PostTagTableSeeder.php
+++ database/seeds/PostTagTableSeeder.php
@@ -6,7 +6,7 @@ use App\Post;
 use App\Tag;
 
 class PostTagTableSeeder extends Seeder
-{
+{   
     /**
      * Run the database seeds.
      *
@@ -14,34 +14,18 @@ class PostTagTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $tags = Tag::all();
+        $tag_ids = Tag::select('id')->get();
         $posts = Post::all();
-        $tags_total_number = count($tags);
+        $x = 75; // Set probability of having at least one tag
+        $y = 40; // Set tag number probability
 
         foreach($posts as $post) {
-            // 75% of chanches
-            if($faker->boolean(75)) { 
-                // Random number for tags in this post
-                $tags_number_for_post = rand(1, 4);
-                $tag_counter = 0;
-                $tag_ids = array();
-
-                // Find unique tag ids (as many as generated random number)
-                do {
-
-                    $temp_id = rand(1, $tags_total_number);
-                    
-                    if(!in_array($temp_id, $tag_ids)) {
-                        $tag_ids[] = $temp_id;
-                    }
-                    
-                } while (count($tag_ids) < $tags_number_for_post);
-
-                // Populate pivot table
-                $post->tags()->attach($tag_ids);
-
-                // Reset array
-                unset($tag_ids);
+            if($faker->boolean($x)) {
+                foreach($tag_ids as $tag_id) {
+                    if($faker->boolean($y)) {
+                        $post->tags()->attach($tag_id);
+                    } 
+                }
             }
         }
     }
