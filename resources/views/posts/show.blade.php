dcfdec24366de9667ebdc39bfa362d680b2e4169
Davide Saporita <davide.saporita@gmail.com>
2020-06-24 17:31:52 +0200

    Added | tags in posts.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,14 +2,26 @@
 
 @section('content')
     <h1>{{ $post->title }}</h1>
-    <h3><a href="{{ route('users.show', $post->user) }}">{{ $post->user->name }}</a></h3>
+    <h4>
+        Author: <a href="{{ route('users.show', $post->user) }}">{{ $post->user->name }}</a>
+    </h4>
+    <h5>Tags:
+        @forelse($post->tags as $tag)
+            <span>{{ $tag->name }}</span>
+            @if(! $loop->last) 
+                <span>|</span>
+            @endif
+        @empty
+            <span>No Tags defined</span>
+        @endforelse
+    </h4>    
     <p>{{ $post->body }}</p>
     <hr>
-    <h4>Comments</h4>
+    <h5>Comments</h5>
     <ul>
         @foreach($comments as $comment)
             <li>
-                <h4>{{ $comment->title }}</h4>
+                <h6>{{ $comment->title }}</h6>
                 <p>{{ $comment->body }}</p> 
                 <span>Author: 
                     <a href="{{ route('users.show', $comment->user) }}">{{ $comment->user->name }}</a>
